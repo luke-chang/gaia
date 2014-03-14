ed6366e98d6934703427d983e3acbc28fde61ccb
Luke Chang <lchang@mozilla.com>
2014-03-14 19:31:45 +0800

    * add app_list toggle button

--- apps/homescreen-tv-arch3/js/homescreen.js
+++ apps/homescreen-tv-arch3/js/homescreen.js
@@ -2,13 +2,27 @@
 
 (function() {
   function init() {
+    var appList = document.getElementById('app_list');
+
     AppList.init(function(docFragment) {
-      document.getElementById('app_list').appendChild(docFragment);
+      document.getElementById('app_list_container').appendChild(docFragment);
     });
 
     document.addEventListener('contextmenu', function(evt) {
       evt.preventDefault();
     });
+
+    document.getElementById('app_list_open_button').addEventListener('click',
+      function() {
+        appList.style.display = 'block';
+      }
+    );
+
+    document.getElementById('app_list_close_button').addEventListener('click',
+      function() {
+        appList.style.display = 'none';
+      }
+    );
   }
 
   window.addEventListener('load', init);
