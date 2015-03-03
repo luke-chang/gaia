b20174afa8edf8d127ae0bf51e54efd5af2df9f7
Luke Chang <lchang@mozilla.com>
2015-03-03 18:23:38 +0800

    * add partner's "contacts.js"

--- shared/js/dialer/contacts.js
+++ shared/js/dialer/contacts.js
@@ -8,7 +8,8 @@ var Contacts = {
   _FB_FILES: [
     '/shared/js/fb/fb_request.js',
     '/shared/js/fb/fb_data_reader.js',
-    '/shared/js/fb/fb_reader_utils.js'
+    '/shared/js/fb/fb_reader_utils.js',
+    '/shared/js/icc_helper.js'//TCL_ZhaoLingling for PR 681598
   ],
 
   // The mozContact API stores a revision of its database that allow us to know
@@ -31,9 +32,143 @@ var Contacts = {
   },
 
   findByNumber: function findByNumber(number, callback) {
-    LazyLoader.load(this._FB_FILES,
-                    this._findByNumber.bind(this, number, callback));
+    //TCL_ZhaoLingling for PR 681598--begin
+    var self = this;
+    var req = navigator.mozSettings.createLock().get('dialer.display.fdnname');
+
+    req.onsuccess = function() {
+      //T2M_lxx modify for PR860695 start
+      if (req.result['dialer.display.fdnname']) {
+        LazyLoader.load(self._FB_FILES,
+          self._findByNumberFDN.bind(self, number, callback));
+      } else {
+        LazyLoader.load(self._FB_FILES,
+          self._findByNumber.bind(self, number, callback));
+      }
+    };
+    //T2M_lxx modify for PR860695 end
+
+    req.onerror = function onerror() {
+      LazyLoader.load(self._FB_FILES,
+        self._findByNumber.bind(self, number, callback));
+    };
+    //TCL_ZhaoLingling for PR 681598--end
   },
+
+  //TCL_ZhaoLingling for PR 681598--begin
+  _findByNumberFDN: function _findByNumberFDN(number, callback) {
+    dump('dialer:_findByNumberFDN');
+    var self = this;
+    //TCL_ZhaoLingling for PR 728947--begin
+    var connections = navigator.mozMobileConnections;
+    if (connections) {
+      var conn = connections[0];
+      if (!conn || !conn.voice || conn.voice.emergencyCallsOnly) {
+        self._findByNumber(number, callback);
+        return;
+      }
+    }
+    //TCL_ZhaoLingling for PR 728947--end
+
+    // alter by xingming.yin.hz@tcl.com; for pr859948;start;
+    var index = 0,
+        active = navigator.mozTelephony.active;
+    //BEGIN-added by tanglin jrd_cd on 20150105 for PR889565
+    for (var i = 0; i < connections.length; i++) {
+      if (connections[i] && connections[i].iccId) {
+        index = i;
+        break;
+      }
+    }
+    //END-added by tanglin jrd_cd on 20150105 for PR889565
+    if (!!active)
+        index = active.serviceId;
+    var iccObject = this.getIccByIndex(index);
+
+    //BEGIN-added by tanglin jrd_cd on 20150105 for PR889565
+    if (!iccObject) {
+      self._findByNumber(number, callback);
+      return;
+    }
+    //END-added by tanglin jrd_cd on 20150105 for PR889565
+
+    var req = iccObject.getCardLock('fdn');
+    req.onsuccess = function onsuccess() {
+      var enabled = req.result.enabled;
+      dump('dialer:getCardLock success,enabled is ' + enabled);
+      if (enabled) {
+        var request = iccObject.readContacts('fdn');
+        // alter by xingming.yin.hz@tcl.com; for pr859948;end;
+        request.onsuccess = function onsuccess() {
+          dump('dialer:readContacts success');
+          // alter by xingming.yin.hz@tcl.com for pr906028
+          var fdnContactsList = request.result.data;
+          var fdnContacts = null;
+          var matchingTel = null;
+          for (var i = 0, l = fdnContactsList.length; i < l; i++) {
+            if (number && number == fdnContactsList[i].tel[0].value) {
+              matchingTel = {
+                type: 'mobile',
+                carrier: null,
+                value: number
+              };
+              fdnContacts = {
+                id: i + 1,
+                name: fdnContactsList[i].name,
+                tel: matchingTel,
+                photo: null
+              };
+              break;
+            }
+          }
+          if (fdnContacts && fdnContacts.name != 'null') {
+            callback(fdnContacts, matchingTel, 0);
+          } else {
+            self._findByNumber(number, callback);
+          }
+        };
+        request.onerror = function onerror() {
+          dump('dialer:readContacts error');
+          callback(null);
+        };
+      } else {
+        self._findByNumber(number, callback);
+      }
+    };
+    req.onerror = function onerror() {
+      dump('dialer:getCardLock error ');
+      self._findByNumber(number, callback);
+    };
+  },
+  //TCL_ZhaoLingling for PR 681598--end
+
+
+  // add by xingming.yin.hz@tcl.com; for pr859948; start
+  /**
+   * Retrieve current ICC by a given index. If no index is provided, it will
+   * use the index provided by `DsdsSettings.getIccCardIndexForCallSettings`,
+   * which is the default. Unless there are very specific reasons to provide an
+   * index, this function should always be invoked with no parameters in order to
+   * use the currently selected ICC index.
+   *
+   * @param {Number} index index of the mobile connection to get the ICC from
+   * @return {object}
+   */
+  getIccByIndex: function(index) {
+      if (index === undefined) {
+        index = 0;
+      }
+      var iccObj;
+
+      if (navigator.mozMobileConnections[index]) {
+         var iccId = navigator.mozMobileConnections[index].iccId;
+         if (iccId) {
+             iccObj = navigator.mozIccManager.getIccById(iccId);
+         }
+      }
+      return iccObj;
+   },
+   // add by xingming.yin.hz@tcl.com; for pr859948; end
 
   _findByNumber: function _findByNumber(number, callback) {
     if (!number) {
@@ -112,6 +247,17 @@ var Contacts = {
       }
 
       var matchingTel = contact.tel[matchResult.localIndex];
+
+      //BEGIN-added by xiaoqin.nie tcl_cd on 20141027 for PR819550
+      var totalMatchNum = matchResult.totalMatchNum;
+      for (var i = 0; i < totalMatchNum; i++) {
+        if (number === contact.tel[i].value) {
+          matchingTel = contact.tel[i];
+          break;
+        }
+      }
+      //END-added by xiaoqin.nie tcl_cd on 20141027 for PR819550
+
       if (fb.isFbLinked(contact)) {
         // Merge with the FB data
         var req = fb.getData(contact);
