756b1cda4bdad2686dc3c808ccb9f930de27cccf
Luke Chang <lchang@mozilla.com>
2015-09-11 19:14:59 +0800

    * hide cursor when pressing ARROW keys or idling for 30 seconds

--- tv_apps/smart-system/js/mouse_cursor.js
+++ tv_apps/smart-system/js/mouse_cursor.js
@@ -2,11 +2,29 @@
 
 window.addEventListener('load', function() {
   var cursor = document.getElementById('mouse-cursor');
-  cursor.style.display = 'block';
+  var timer;
 
   window.addEventListener('remote-control-event', function(evt) {
     var detail = evt.detail;
+    cursor.style.display = 'block';
     cursor.style.MozTransform =
       'translateX(' + detail.x + 'px) translateY(' + detail.y + 'px)';
+
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function() {
+      cursor.style.display = 'none';
+    }, 30000);
+  });
+
+  window.addEventListener('mozbrowserbeforekeydown', function(evt) {
+    if (evt.keyCode >= 37 && evt.keyCode <= 40) {
+      if (timer) {
+        clearTimeout(timer);
+        timer = undefined;
+      }
+      cursor.style.display = 'none';
+    }
   });
 });
