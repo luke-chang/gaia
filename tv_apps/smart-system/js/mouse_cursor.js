aa26816e3c26b6eb4f9cf40478e09a4a2c29dca7
Luke Chang <lchang@mozilla.com>
2015-09-14 14:45:48 +0800

    * optimize mouse cursor

--- tv_apps/smart-system/js/mouse_cursor.js
+++ tv_apps/smart-system/js/mouse_cursor.js
@@ -6,20 +6,29 @@ window.addEventListener('load', function() {
 
   window.addEventListener('remote-control-event', function(evt) {
     var detail = evt.detail;
+    if (detail.action != 'move-cursor') {
+      return;
+    }
+
     cursor.style.display = 'block';
     cursor.style.MozTransform =
       'translateX(' + detail.x + 'px) translateY(' + detail.y + 'px)';
 
     if (timer) {
       clearTimeout(timer);
+      timer = undefined;
     }
-    timer = setTimeout(function() {
-      cursor.style.display = 'none';
-    }, 30000);
+
+    if (detail.state == 'end') {
+      timer = setTimeout(function() {
+        cursor.style.display = 'none';
+      }, 30000);
+    }
   });
 
   window.addEventListener('mozbrowserbeforekeydown', function(evt) {
-    if (evt.keyCode >= 37 && evt.keyCode <= 40) {
+    var keyCode = evt.keyCode;
+    if (keyCode >= 37 && keyCode <= 40) {
       if (timer) {
         clearTimeout(timer);
         timer = undefined;
