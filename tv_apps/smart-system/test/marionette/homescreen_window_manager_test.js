1541a3c44bd323e1af124c8de7f04b8783099411
Luke Chang <lchang@mozilla.com>
2016-02-25 20:51:16 +0800

    * update

--- tv_apps/smart-system/test/marionette/homescreen_window_manager_test.js
+++ tv_apps/smart-system/test/marionette/homescreen_window_manager_test.js
@@ -3,6 +3,13 @@
 //var assert = require('chai').assert;
 
 marionette('Test Homescreen Window Manager', function() {
+  /*var Keys = {
+    'enter': '\ue006',
+    'right': '\ue014',
+    'esc': '\ue00c',
+    'backspace': '\ue003'
+  };*/
+
   var opts = {
     apps: {},
     hostOptions: {
@@ -19,15 +26,23 @@ marionette('Test Homescreen Window Manager', function() {
     desiredCapabilities: { raisesAccessibilityExceptions: false }
   });
   var testOptions = { devices: ['tv'] };
-  var system;
+  var system, home;
+
+  var arbitraryAppUrl = 'app://app-deck.gaiamobile.org';
 
   setup(function() {
     system = client.loader.getAppClass('system');
+    home = client.loader.getAppClass('smart-home', 'home', 'tv_apps');
     system.waitForStartup();
     system.waitForFullyLoaded();
+    home.switchFrame();
+    home.skipFte();
+    client.switchToFrame();
   });
 
   test('a sample test', testOptions, function() {
-
+    // Launch test app
+    var frame = system.waitForLaunch(arbitraryAppUrl);
+    client.switchToFrame(frame);
   });
 });
